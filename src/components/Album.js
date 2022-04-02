7e2a79e38013057510ef2617dae8cd83e1f45cd7
Suyash Wagh <suyashcr07@gmail.com>
2022-04-02 07:57:13 +0530

    Add responsiveness

--- src/components/Album.js
+++ src/components/Album.js
@@ -2,11 +2,11 @@ const Album = (props) => {
 	const { userId, id, title, handleDelete, handleUpdate } = props;
 
   return (
-		<li className='Album w-1/5 p-2 grow bg-[rgba(255,255,255,0.8)] rounded'>
+		<li className='Album w-2/5 lg:w-1/5 p-2 grow bg-[rgba(255,255,255,0.8)] rounded'>
 			<h1 className='font-bold truncate'>{title}</h1>
 			<p>User ID: <b>{userId}</b></p>
 			<p>Album ID: <b>{id}</b></p>
-			<div className='flex justify-between items-center'>
+			<div className='flex flex-col md:flex-row justify-between items-center'>
 				<button className='mt-4 px-4 py-2 text-blue-500 border-2 border-blue-500 hover:bg-blue-500 hover:text-white' data-title={title} data-userid={userId} data-albumid={id} onClick={handleUpdate}>Update</button>
 				<button className='mt-4 px-4 py-2 text-red-500 border-2 border-red-500 hover:bg-red-500 hover:text-white' id={id} onClick={handleDelete}>Delete</button>
 			</div>
--- src/components/Albums.js
+++ src/components/Albums.js
@@ -4,12 +4,12 @@ const Albums = (props) => {
 	const { albums, handleAdd, handleDelete, handleUpdate } = props;
 
   return (
-		<div className='Albums w-3/4 flex flex-col justify-center items-center gap-4 m-8'>
+		<div className='Albums w-full lg:w-4/5 flex flex-col justify-center items-center gap-4 m-8'>
 			<div className='w-full px-4 flex justify-between items-center'>
 				<h1 className='text-3xl text-white font-bold'>Albums</h1>
 				<button className='mt-4 px-4 py-2 text-white bg-green-500 hover:bg-green-600 hover:text-white' onClick={handleAdd}>Add</button>
 			</div>
-			<ul className='flex flex-wrap justify-evenly items-center gap-4 p-4'>
+			<ul className='w-full flex flex-wrap justify-evenly items-center gap-4 p-4'>
 				{
 					albums.map((album) => (
 						<Album userId={album.userId} id={album.id} title={album.title} key={album.id} handleDelete={handleDelete} handleUpdate={handleUpdate} />
